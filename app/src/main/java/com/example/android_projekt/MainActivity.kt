753b14d3cfcfbe308c6b09687f6337d78414d15b
Szaki <szakaliarpi@yahoo.com>
2020-12-10 20:01:02 +0200

    navigation bar in progress...

--- app/src/main/java/com/example/android_projekt/MainActivity.kt
+++ app/src/main/java/com/example/android_projekt/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.android_projekt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottomNav)
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod)
+
+
     }
+
+    private val bottomNavMethod = BottomNavigationView.OnNavigationItemSelectedListener { false }
 }

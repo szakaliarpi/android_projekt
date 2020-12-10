ab74f34cd36b70f3c2ca053c2e186beed95de940
Szaki <szakaliarpi@yahoo.com>
2020-12-10 18:26:47 +0200

    Splash screen done

---
+++ app/src/main/java/com/example/android_projekt/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.android_projekt
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val myThread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(2500)
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        myThread.start()
+    }
+}
